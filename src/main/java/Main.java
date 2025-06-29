968fcc77a75b175e5224c94bdfa9cc9e7aacca37
S.SELVA THARRUN <82883200+selvatharrun@users.noreply.github.com>
2025-06-29 06:26:10 +0000

    hmm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,17 @@ public class Main {
         lst.add("I love nuggets");
         lst.add("I love burger");
         lst.add("burger is great");
-        
-        Map<String,Integer> map = new HashMap<>();
-        List<Map<String,Integer>> lst2 = new ArrayList<>();
 
-        for(String s:lst){
+        Map<String, Integer> map = new HashMap<>();
+        List<Map<String, Integer>> lst2 = new ArrayList<>();
+
+        for (String s : lst) {
             String[] words = s.split(" ");
-            for(int i = 0;i<words.length;i++){
-                map.put(words[i],map.getOrDefault(0,map.get(words[i])+1));
+            for (int i = 0; i < words.length; i++) {
+                map.put(words[i], map.getOrDefault(0, map.get(words[i]) + 1));
             }
             lst2.add(map);
         }
         System.out.println(lst2);
     }
 }
-
