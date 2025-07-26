72edba42aa0bd56996c13212381c03135e5d8572
S.SELVA THARRUN <82883200+selvatharrun@users.noreply.github.com>
2025-07-26 14:40:05 +0000

    slight changes and lot of commenting, bubblesort added

--- src/main/java/DSA/bubblesort.java
+++ src/main/java/DSA/bubblesort.java
@@ -1,4 +1,4 @@
-public class Main {
+public class bubblesort {
     public void swap (int[] lst , int a, int b){
         int temp = lst[b];
         lst[b] = lst[a];
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         int[] lst = {2,3,4,1,6,5,7,8,9};
 
-        Main obj = new Main();
-        
+        bubblesort obj = new bubblesort();
+
         for(int i=1;i<lst.length;i++){
             for(int j=0; j < lst.length-1 ; j++){
                 if(lst[j] > lst[j+1]){
