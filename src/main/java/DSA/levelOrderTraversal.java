17d23be124951ed7df05ca10aa79bd9fa1fc1cf2
S.SELVA THARRUN <82883200+selvatharrun@users.noreply.github.com>
2025-08-04 09:29:51 +0000

    uncommented level order traversal lol

--- src/main/java/DSA/levelOrderTraversal.java
+++ src/main/java/DSA/levelOrderTraversal.java
@@ -1,44 +1,51 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
 
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> mainlst = new ArrayList<>();
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-//         if(root == null){
-//             return mainlst;
-//         }
+    TreeNode() {
+    }
 
-//         Queue<TreeNode> q = new LinkedList<>();
-//         q.offer(root);
+    TreeNode(int val) {
+        this.val = val;
+    }
 
-//         while(!q.isEmpty()){
-//             List<Integer> level = new ArrayList<>();
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
-//             int levelSize = q.size();
-//             for(int i=0; i<levelSize; i++){
-//                 TreeNode curr = q.poll();
-//                 level.add(curr.val);
+public class levelOrderTraversal {
 
-//                 if(curr.left!=null) q.add(curr.left);
-//                 if(curr.right!=null)q.add(curr.right);
-//             }
-//             mainlst.add(level);
-//         }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> mainlst = new ArrayList<>();
 
-//         return mainlst;
-//     }
-// }
+        if (root == null) {
+            return mainlst;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+            mainlst.add(level);
+        }
+
+        return mainlst;
+    }
+}
